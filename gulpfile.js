0c66c37120b8484ab8b2045dde590b1bfd1d9942
Caio Santos <caio.f.santos@icloud.com>
2019-02-18 17:57:55 -0300

    Basic boilerplate created
    - File structure created
    - Sass 7x1 structure created
    - Basic gulpfile.js created with html minify and sass compile functions

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const htmlmin = require('gulp-htmlmin');
+
+gulp.task('default', gulp.series(watchFiles))
+
+// FUNCTIONS
+function htmlMin() {
+    return gulp
+        .src('src/index.html')
+        .pipe(htmlmin(
+            {
+                collapseWhitespace: true
+            }
+        ))
+        .pipe(gulp.dest('dist'));
+}
+
+function compileSass() {
+    return gulp
+        .src('src/sass/main.scss')
+        .pipe(sass(
+            {
+                outputStyle: 'compressed'
+            }
+        ))
+        .pipe(gulp.dest('dist/css/'))
+}
+
+// WATCH
+function watchFiles() {
+    gulp.watch('.src/index.html', htmlMin);
+    gulp.watch('./src/sass/main.scss', compileSass);
+}
+
+// Exporting tasks
+exports.htmlMin = htmlMin;
+exports.compileSass = compileSass;
